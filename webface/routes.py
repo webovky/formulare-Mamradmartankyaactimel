fdaf390fe6699e84a886ff639052ab993d5f3b25
Mamradmartankyaactimel <nov37183@spseol.cz>
2021-01-24 19:36:13 +0100

    anooo

--- webface/routes.py
+++ webface/routes.py
@@ -17,6 +17,56 @@ def prihlasit(function):
 
     return wrapper
 
+@app.route("/trojuhelnikOS/")
+def trojuhelnik():
+    title = "Trojuhlnik"
+    a = request.args.get("stranaa")
+    b = request.args.get("stranab")
+    c = request.args.get("stranac")
+    try:
+        o = int(a) + int(b) + int(c)
+    except (TypeError, ValueError):
+        o = ''
+    return render_template("trojuhelnikOS.html.j2", title=title, o=o)
+
+@app.route("/ctverecOS/")
+def ctverec():
+    title = "Ctverec"
+    a = request.args.get("stranaa")
+    try:
+        o = 4 * int(a)
+        s = int(a) * int(a)
+    except (TypeError, ValueError):
+        o = ''
+        s = ''
+    return render_template("ctverecOS.html.j2", title=title, o=o, s=s)
+
+@app.route("/kruhOS/")
+def kruh():
+    title = "kruh"
+    a = request.args.get("a")
+    try:
+        o = (int(a)*int(a)) * 3.14
+        s = int(a) * 2 * 3.14 
+    except (TypeError, ValueError):
+        o = ''
+        s = ''
+    return render_template('kruhOS.html.j2', title=title,o=o,s=s)    
+
+@app.route("/obdelnik/")
+def obdelnik():
+    title = "Obdelnik"
+    a = request.args.get("a")
+    b = request.args.get("b")
+    try:
+        o = 2* (int(a)+int(b))
+        s = int(a) * int(b)
+    except (TypeError, ValueError):
+        o = ''
+        s = ''
+    return render_template("obdelnikOS.html.j2", title=title, o=o, s=s)
+
+
 
 @app.route("/", methods=["GET"])
 def index():
